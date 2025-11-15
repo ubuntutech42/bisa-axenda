e86d43f55cc1ec69424b5caa6008a86255ddb8e8
Ubuntu Tech <ubuntutech42@gmail.com>
2025-11-15 00:24:40 +0000

    Animação mais lenta (_for element <Card>_)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -96,7 +96,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'float': 'float 6s ease-in-out infinite',
+        'float': 'float 12s ease-in-out infinite',
       },
     },
   },
