39b1776e0c242b6b87ebc6fa268e4d05064e500e
Ubuntu Tech <ubuntutech42@gmail.com>
2025-11-15 00:45:06 +0000

    Torne a largura do calendário a mesma largura da div pai (_for element <

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -18,7 +18,7 @@ function Calendar({
   return (
     <DayPicker
       showOutsideDays={showOutsideDays}
-      className={cn(className)}
+      className={cn("p-3", className)}
       classNames={{
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
@@ -34,9 +34,9 @@ function Calendar({
         table: "w-full border-collapse space-y-1",
         head_row: "flex justify-between",
         head_cell:
-          "text-muted-foreground rounded-md w-12 font-normal text-sm",
+          "text-muted-foreground rounded-md w-full font-normal text-sm",
         row: "flex w-full mt-2 justify-between",
-        cell: "h-12 w-12 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
+        cell: "h-12 w-full text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
         day: cn(
           buttonVariants({ variant: "ghost" }),
           "h-12 w-12 p-0 font-normal aria-selected:opacity-100"
