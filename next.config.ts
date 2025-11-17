b026c4345b3a92eafc8660f4a87ac120bc75bd41
Ubuntu Tech <ubuntutech42@gmail.com>
2025-11-17 18:36:56 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -30,6 +31,11 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  experimental: {
+    allowedDevOrigins: [
+        "https://6000-firebase-studio-1759897343276.cluster-zhw3w37rxzgkutusbbhib6qhra.cloudworkstations.dev"
+    ]
+  }
 };
 
 export default nextConfig;
