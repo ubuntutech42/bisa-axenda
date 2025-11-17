e2574ad1474b8f1a0e210c6a0954adef4ca41ecc
Ubuntu Tech <ubuntutech42@gmail.com>
2025-11-17 18:38:04 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -24,10 +24,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
+        protocol: 'https'
       },
     ],
   },
